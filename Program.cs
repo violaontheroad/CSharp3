08f179f2181ea3d88912336593fca8e74a595797
Viola <violaonther@gmail.com>
2022-05-11 15:42:26 -0300

    Introdução à orientação de objetos

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿
+ContaCorrente contaDoBruno = new ContaCorrente();
+
+contaDoBruno.titular = "Bruno";
+
+Console.WriteLine(contaDoBruno.saldo);
+
+bool resultadoSaque = contaDoBruno.Sacar(50);
+
+Console.WriteLine(contaDoBruno.saldo); 
+Console.WriteLine(resultadoSaque);
+
+
+contaDoBruno.Depositar(500);
+Console.WriteLine(contaDoBruno.saldo); 
+
+Console.ReadLine();
+
+
+
+
