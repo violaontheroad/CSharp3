5cca46151ff286bcbed4fde96cfac73827f57a1a
Viola <violaonther@gmail.com>
2022-05-16 10:54:31 -0300

    intro orientação de objetos

--- ContaCorrente.cs
+++ ContaCorrente.cs
@@ -3,7 +3,7 @@ public class ContaCorrente
     public string titular;
     public int agencia;
     public int numero;
-    public double saldo = 125;
+    public double saldo = 400;
 
     public bool Sacar(double valor)
     {
@@ -22,4 +22,17 @@ public class ContaCorrente
        this.saldo += valor;
     }
 
+    public bool Transferir(double valor, ContaCorrente contaDestino)
+    {
+        if(this.saldo < valor)
+        {
+            return false;
+        }
+        else
+        {
+            this.saldo -= valor;
+            contaDestino.Depositar(valor);
+            return true;
+        }
+    }
 }
--- Program.cs
+++ Program.cs
@@ -2,17 +2,24 @@
 ContaCorrente contaDoBruno = new ContaCorrente();
 
 contaDoBruno.titular = "Bruno";
-
-Console.WriteLine(contaDoBruno.saldo);
-
 bool resultadoSaque = contaDoBruno.Sacar(50);
 
-Console.WriteLine(contaDoBruno.saldo); 
 Console.WriteLine(resultadoSaque);
-
+Console.WriteLine(contaDoBruno.saldo); 
 
 contaDoBruno.Depositar(500);
 Console.WriteLine(contaDoBruno.saldo); 
+
+// implementando a função Transferir
+
+ContaCorrente contaDoViola = new ContaCorrente();
+
+contaDoViola.titular = "Viola";
+
+contaDoBruno.Transferir(300, contaDoViola);
+
+Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+Console.WriteLine("Saldo do Viola: " + contaDoViola.saldo);
 
 Console.ReadLine();
 
